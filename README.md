b481f447766d0eb0a218fe38237bae7fc803d929
Yonatan <powchymc@gmail.com>
2020-02-23 20:50:00 +0200

    Added arch support

--- README.md
+++ README.md
@@ -3,12 +3,17 @@
 ## Weather
 Simple tool made to check the weather on your local/remote terminal.
 
-## Installation
+## Installation - Ubuntu/Debian
 
 	git clone https://github.com/Sorikal/Weather.git
 	cd Weather
 	python3 weather.py
 
+## Installation - Arch Linux
+
+	git clone https://github.com/Sorikal/Weather.git
+	cd Weather
+	python3 weatherarch.py
 
 ## Credit
 
---
+++ weatherarch.py
@@ -0,0 +1,26 @@
+import os
+import time
+
+### Checking for dependencies
+
+print("Checking for missing dependencies.")
+os.system("sudo pacman -Qi curl > log.txt")
+
+with open('log.txt', 'r') as file:
+	dep = file.read().replace('\n', '')
+
+if "was not found" in dep:
+	os.system("sudo pacman -S curl")
+else:
+	os.system("clear")
+	print("All dependencies are present.")
+	os.system("clear")
+
+os.system("rm -rf log.txt")
+
+### Curling weather
+
+os.system("curl wttr.in")
+
+
+### Report any issues on Github.
