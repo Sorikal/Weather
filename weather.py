bd1f07c8b127233e2cf8b31e3090ce66dffe8b53
Yonatan <powchymc@gmail.com>
2020-02-27 19:49:09 +0200

    Code improvments.

--- weather.py
+++ weather.py
@@ -1,26 +1,24 @@
 import os
 
-#### This program will parse the weather in your area and dispay it to you
-### This tool is using wttr.in for weather
-
-### Check for dependencies
-
-os.system("clear")
-
-os.system("dpkg -l | grep -E '^ii' | grep curl > log.txt")
-with open('log.txt', 'r') as file:
-    cmdlog = file.read().replace('\n', '')
-
-os.system("rm -rf log.txt")
-
-if cmdlog == "":
-    print("We have to install some depenencies...")
-    os.system("sudo apt install curl")
-    os.system("clear")
-else:
+def cls():
     os.system("clear")
 
-os.system("curl wttr.in")
+def Dependencies():
+
+    os.system("dpkg -l | grep -E '^ii' | grep curl > log.txt")
+    with open('log.txt', 'r') as file:
+        cmdlog = file.read().replace('\n', '')
+
+    os.system("rm -rf log.txt")
+
+    if cmdlog == "":
+        os.system("sudo apt install curl")
+        cls()
+    else:
+        cls()
+
+def CurlWeather():
+    os.system("curl wttr.in")
 
 
 
--- weatherarch.py
+++ weatherarch.py
@@ -1,26 +1,30 @@
 import os
-import time
 
-### Checking for dependencies
-
-print("Checking for missing dependencies.")
-os.system("pacman -Qi curl > log.txt")
-
-with open('log.txt', 'r') as file:
-	dep = file.read().replace('\n', '')
-
-if "was not found" in dep:
-	os.system("sudo pacman -S curl")
-else:
-	os.system("clear")
-	print("All dependencies are present.")
+def cls():
 	os.system("clear")
 
-os.system("rm -rf log.txt")
+def Dependencies():
 
-### Curling weather
+	print("Checking for missing dependencies.")
+	os.system("pacman -Qi curl > log.txt")
 
-os.system("curl wttr.in")
+	with open('log.txt', 'r') as file:
+		dep = file.read().replace('\n', '')
 
+	if "was not found" in dep:
+		os.system("sudo pacman -S curl")
+	else:
+		cls()
+		print("All dependencies are present.")
+		cls()
+		os.system("rm -rf log.txt")
+
+def CurlWeather():
+
+	os.system("curl wttr.in")
+
+
+Dependencies()
+CurlWeather()
 
 ### Report any issues on Github.
